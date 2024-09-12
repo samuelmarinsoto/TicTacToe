27d320106b2825074749f71133e172b9663fb9c6
Samuel Marín Soto <samarin@estudiantec.cr>
2024-09-12 01:05:21 -0600

    no habia que encontrar una diagonal completa, solo 3x3

--- gato-op.rkt
+++ gato-op.rkt
@@ -2,64 +2,47 @@
 
 (require "matriz-op.rkt")
 
-(define (diag-no? matriz marcador m n)
+(define (diagonal? matriz marcador m n)
 	(cond
-		(cond
-			((> n m)
-				(diag-no? matriz marcador m m))
-			((> m n)
-				(diag-no? matriz marcador n n))
-			(else
-				((zero? m) #t)
-				(else
-					(cond
-						((not (equal? marcador (matriz-buscar m m))) 
-							#f)
-						(else
-							(diag-no? matriz marcador (- m 1) (- m 1)))
-					)
-				)
-			)				
-		)		
-	)
-)
-
-(define (diag-ne? matriz marcador m n)
-	(define (diag-ne-aux? j i)
-		(cond
-			((= i n) #t)
-			((< j 0) #t)
-			(else
-				(cond
-					((not (equal? marcador (matriz-buscar j i)))
-						#f)
-					(else
-						(diag-ne-aux (- j 1) (+ i 1)))
-				)
+		(or
+			(and
+				(equal? marcador (matriz-buscar m n))
+				(equal? marcador (matriz-buscar (+ m 1) (+ n 1)))
+				(equal? marcador (matriz-buscar (- m 1) (- n 1)))
+			)
+			(and
+				(equal? marcador (matriz-buscar m n))
+				(equal? marcador (matriz-buscar (- m 1) (+ n 1)))
+				(equal? marcador (matriz-buscar (+ m 1) (- n 1)))
 			)
 		)
 	)
-
-	(diag-ne-aux? (- m 1) 0)
 )
+			
+	
 
-(define (diagonal? matriz marcador m n)
+(define (gane? matriz marcador m n)
 	(cond
 		((>= m (length (car matriz)))
 			(error "Indice afuera de vector"))
 		((>= n (length matriz))
 			(error "Indice afuera de vector de vectores"))
-		(else
-			(or
-				(diag-no? matriz marcador m n)
-				(diag-ne? matriz marcador m n)
-				(diag-so? matriz marcador m n)
-				(diag-se? matriz marcador m n)
+		(define (gane-aux? j i)
+			((zero? i) #f)
+			(else
+				(cond
+					((zero? j)
+						(gane-aux? m (- i 1)))
+					(else
+						(diagonal? matriz marcador j i))
+				)
 			)
 		)
+		
+		(gane-aux? (- m 1) (- n 1))
 	)
 )
-
+	
 ; asume que el marcador de "vacio" es #f
 (define (empate? matriz)
 	(cond
