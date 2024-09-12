0785d1ed319b92ace7a5f12bd2008854a37b4aa2
Samuel Marín Soto <samarin@estudiantec.cr>
2024-09-11 21:17:35 -0600

    checkeo de diagonal noreoeste

--- gato-op.rkt
+++ gato-op.rkt
@@ -2,9 +2,32 @@
 
 (require "matriz-op.rkt")
 
-;(define (diag-no? matriz marcador m n)
-;	((equal? marcador (matriz-buscar m n) #t)
-		
+(define (diag-no? matriz marcador m n)
+	(cond
+		(cond
+			((> n m)
+				(diag-no? matriz marcador m m))
+			((> m n)
+				(diag-no? matriz marcador n n))
+			(else
+				((zero? m) #t)
+				(else
+					(cond
+						((not (equal? marcador (matriz-buscar m m))) 
+							#f)
+						(else
+							(diag-no? matriz marcador (- m 1) (- m 1)))
+					)
+				)
+			)				
+		)		
+	)
+)
+
+(define (diag-ne? matriz marcador n)
+	(cond
+		(
+
 (define (diagonal? matriz marcador m n)
 	(cond
 		((>= m (length (car matriz)))
@@ -13,7 +36,7 @@
 			(error "Indice afuera de vector de vectores"))
 		(else
 			(or
-				(diag-no? matriz marcador m n) ; por definir
+				(diag-no? matriz marcador m n)
 				(diag-ne? matriz marcador m n)
 				(diag-so? matriz marcador m n)
 				(diag-se? matriz marcador m n)
